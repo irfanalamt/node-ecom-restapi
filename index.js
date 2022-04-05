5b948c7ffcb7ee0f83119d06d934946d67a2e640
Irfan <irfanalamt@gmail.com>
2022-04-05 19:52:33 -0400

    Mongo connection success.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("Mongoose connection successfull.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(5000, () => {
   console.log("Express server running on port 5000.");
 });
