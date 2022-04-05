032067d799f655579544a785e3eedb57c739aa69
Irfan <irfanalamt@gmail.com>
2022-04-05 19:41:13 -0400

    Express server started

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-//Im building something awesome!
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.listen(5000, () => {
+  console.log("Express server running on port 5000.");
+});
