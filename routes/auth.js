5740d7edf03c65dda3f8a4add8e03d36774c56bb
Irfan <irfanalamt@gmail.com>
2022-04-05 22:35:30 -0400

    Feat: add login route and credential check

--- routes/auth.js
+++ routes/auth.js
@@ -15,16 +15,37 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     console.log(savedUser);
-    // console.log(
-    //   Crypto.AES.decrypt(savedUser.password, process.env.PASSPHRASE).toString(
-    //     Crypto.enc.Utf8
-    //   )
-    // );
+    console.log(
+      Crypto.AES.decrypt(savedUser.password, process.env.PASSPHRASE).toString(
+        Crypto.enc.Utf8
+      )
+    );
 
     res.status(201).json(savedUser);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(500).json("Invalid credentials");
+      return;
+    }
+    const pass = Crypto.AES.decrypt(
+      user.password,
+      process.env.PASSPHRASE
+    ).toString(Crypto.enc.Utf8);
+    if (pass !== req.body.password) {
+      res.status(500).json("Invalid password");
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
