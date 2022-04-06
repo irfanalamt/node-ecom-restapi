d4d9e16ebb96117946479b46fdccbc4292555d88
Irfan <irfanalamt@gmail.com>
2022-04-05 22:04:02 -0400

    Fix: add password encryption before storage

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Crypto = require("crypto-js");
 
 router.post("/register", async (req, res) => {
   const newUser = User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: Crypto.AES.encrypt(
+      req.body.password,
+      process.env.PASSPHRASE
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     console.log(savedUser);
+    // console.log(
+    //   Crypto.AES.decrypt(savedUser.password, process.env.PASSPHRASE).toString(
+    //     Crypto.enc.Utf8
+    //   )
+    // );
+
     res.status(201).json(savedUser);
   } catch (err) {
     console.log(err);
