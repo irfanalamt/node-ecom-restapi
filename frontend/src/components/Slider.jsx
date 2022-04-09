100eb3b6280040ac019965668254e249ce5fd816
Irfan <irfanalamt@gmail.com>
2022-04-09 13:05:17 -0400

    Style: complete slider arrows

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,10 +1,43 @@
-import { Container } from "@mui/material";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import { Box, Container } from "@mui/material";
+
+const containerStyle = () => {
+  return {
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "#ffcc80",
+    display: "flex",
+  };
+};
+const boxStyle = () => {
+  return {
+    height: "45px",
+    width: "45px",
+    backgroundColor: "#e0f2f1",
+    borderRadius: "50%",
+    margin: "2px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+};
+const arrowStyle = () => {
+  return {
+    fontSize: "35",
+    margin: "2px",
+  };
+};
 
 const Slider = () => {
   return (
-    <Container
-      sx={{ width: "100%", height: "100vh", backgroundColor: "blue" }}
-    ></Container>
+    <Container sx={containerStyle}>
+      <Box sx={boxStyle}>
+        <ArrowLeftOutlined sx={arrowStyle} />
+      </Box>
+      <Box sx={boxStyle}>
+        <ArrowRightOutlined sx={arrowStyle} />
+      </Box>
+    </Container>
   );
 };
 
