e636a0be5c64e007f58d3584ddf089a4312f150d
Irfan <irfanalamt@gmail.com>
2022-06-20 19:37:26 -0400

    Fix: move import to top

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
