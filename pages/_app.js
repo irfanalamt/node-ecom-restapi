b42268ac1c11ff31c4954112e0563ae6058d2263
Irfan <irfanalamt@gmail.com>
2022-06-20 19:52:08 -0400

    Fix: add useEffect to handle clientside objects

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,12 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Layout from '../components/Layout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
