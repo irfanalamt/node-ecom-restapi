179e808431e627bf42b9521263e99bf60c6d21e6
Irfan <irfanalamt@gmail.com>
2022-06-20 21:41:15 -0400

    Fix: added _document

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css'
-          rel='stylesheet'
-          integrity='sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor'
-          crossorigin='anonymous'
-        ></link>
       </Head>
       <Script
         src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'
