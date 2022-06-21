1d3f3659a8c71fed195275b05570821ff8d7ba3e
Irfan <irfanalamt@gmail.com>
2022-06-20 21:30:36 -0400

    Fix: try bootstrap CDN

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,29 @@
 import '../styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import Layout from '../components/Layout';
-import { useEffect } from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    typeof document !== undefined
-      ? require('bootstrap/dist/js/bootstrap')
-      : null;
-  }, []);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css'
+          rel='stylesheet'
+          integrity='sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor'
+          crossorigin='anonymous'
+        ></link>
+      </Head>
+      <Script
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'
+        integrity='sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2'
+        crossorigin='anonymous'
+      ></Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
