ffac3aa73cff59816a4deffbf063c829a9e8b10a
Irfan <irfanalamt@gmail.com>
2022-06-20 21:44:25 -0400

    Fix: camelCase crossOrigin attribute

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Script
         src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'
         integrity='sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2'
-        crossorigin='anonymous'
+        crossOrigin='anonymous'
       ></Script>
       <Layout>
         <Component {...pageProps} />
