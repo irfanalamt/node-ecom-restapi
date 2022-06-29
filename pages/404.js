75f36dc2598f7136bd6d8372d07a10094c7e701c
Irfan <irfanalamt@gmail.com>
2022-06-28 21:56:44 -0400

    Feat: add 404 error page

---
+++ pages/404.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Head from 'next/head';
+import { Chip, Typography } from '@mui/material';
+import { Container } from '@mui/system';
+import ErrorIcon from '@mui/icons-material/Error';
+
+const Custom404 = () => {
+  return (
+    <>
+      <Head>
+        <title>404 ERROR</title>
+      </Head>
+      <Container sx={{ textAlign: 'center', paddingTop: 20 }}>
+        <ErrorIcon sx={{ fontSize: 50, color: 'red' }} />
+        <Typography variant='h1' component='div'>
+          OOPS!
+        </Typography>
+        <Typography variant='subtitle1' gutterBottom component='div'>
+          We can't seem to find the page you are looking for.
+        </Typography>
+        <Typography variant='body2' gutterBottom>
+          Error code:404
+        </Typography>
+      </Container>
+    </>
+  );
+};
+
+export default Custom404;
